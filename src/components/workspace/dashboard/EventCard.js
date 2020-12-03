dc18faa218c1f103f3ea0149bb781ee5dec1c2d7
unclebay <unclebigbay@gmail.com>
2020-12-03 21:58:53 +0100

    convert todo list reminder, eschedule reminder, and event reminder cards on dashboard into components and are implemented an array of objects for test

--- src/components/workspace/dashboard/EventCard.js
+++ src/components/workspace/dashboard/EventCard.js
@@ -1,17 +1,40 @@
-const EventCard = (props) => {
-    const { eventName, eventDate } = this.props
-    return(
-        <>
-            <div class="flex-grow-1 d-flex align-items-center">
-                <div class={`dot mr-3 ${bg-green}`}></div>
-                <div class="text">
-                    <h6 class="mb-0">{eventName}</h6><span class="text-gray">{eventDate}</span>
-                </div>
-            </div>
+import { Component } from "react";
 
-        </>
-    )
-}
+class EventCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            event: [
+                {
+                    title: "I will be coming home soon", 
+                    eventDate: Date.now()
+                },
+                {
+                    title: "2020 TIIDELab Hackathon",
+                    eventDate: "30 Dec 2020"
+                },
+
+            ]
+        }
+    }
+
+    render(){
+        const { event } = this.state;
+        return(
+            <>
+                <div class="bg-white shadow pace-roundy px-4 py-3 d-flex align-items-center justify-content-between mb-4">
+                    <div class="flex-grow-1 d-flex align-items-center">
+                        <div class="dot mr-3 bg-green"></div>
+                        <div class="text">
+                            <h6 class="mb-0">{event[0].title}</h6><span class="text-gray">{event[0].eventDate}</span>
+                        </div>
+                    </div>
+                </div>
+
+            </>
+        )
+    };
+};
 
 
 export default EventCard;
--- src/components/workspace/dashboard/TodoCard.js
+++ src/components/workspace/dashboard/TodoCard.js
@@ -1,17 +1,31 @@
-const TodoCard = (props) => {
-    const { title, dueDate, cardColor } = this.props
-    return(
-        <>
-            <div class="flex-grow-1 d-flex align-items-center">
-                <div class={`dot mr-3 ${cardColor}`}></div>
-                <div class="text">
-                    <h6 class="mb-0">{title}</h6><span class="text-gray">{dueDate}</span>
-                </div>
-            </div>
+import { Component } from "react";
 
-        </>
-    )
-}
+class TodoCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            title: "your todo is currently empty",
+            dueDate: Date.now()
+        }
+    }
+
+    render(){
+        const { title, dueDate, cardColor } = this.state;
+        return(
+            <>
+                <div class="bg-white shadow pace-roundy px-4 py-3 d-flex align-items-center justify-content-between mb-4">
+                    <div class="flex-grow-1 d-flex align-items-center">
+                        <div class="pace-bg-accent dot mr-3"></div>
+                        <div class="text">
+                            <h6 class="mb-0">{title}</h6><span class="text-gray">{dueDate}</span>
+                        </div>
+                    </div>
+                </div>
+
+            </>
+        )
+    };
+};
 
 
 export default TodoCard;
