6fd08baaad78de75b76eb9e3dc1e6d39b702df2c
Aeesh <adamaisha352@gmail.com>
2020-12-16 23:52:37 +0100

    resolved conflict

--- src/components/workspace/dashboard/Dashboard.js
+++ src/components/workspace/dashboard/Dashboard.js
@@ -16,41 +16,7 @@ import EmployeeTasks from './employeeTasks/EmployeeTasks';
 import TimerReport from './reports/TimerReport';
 import BillingReport from './reports/BillingReport';
 import Profile from './Profile';
-<<<<<<< HEAD
 import Task from './Task';
-
-
-class Dashboard extends Component{
-    render(){
-        return(
-            <>
-                <div>
-                   {/* >>>>> NAVBAR COMPONENT SECTION <<<<< */}
-                    <Navbar  />
-                        <div className="d-flex align-items-stretch">
-                   {/* >>>>> SIDEBAR COMPONENT SECTION <<<<< */}
-                            <Sidebar />
-                            <div className="page-holder w-100 d-flex flex-wrap">
-
-                                <div className="container-fluid dashboard-body-wrapper">
-                   {/* >>>>> BODIES COMPONENTS SECTION <<<<< */}
-                                    <Switch>
-                                        <Route exact path="/dashboard/task" component={Task} />
-                                        <Route exact path="/dashboard/profile" component={Profile} />
-                                        <Route exact path="/dashboard/billing-report" component={BillingReport} />
-                                        <Route exact path="/dashboard/timer-report" component={TimerReport} />
-                                        {/* <Route exact path="/dashboard/task" component={EmployeeTasks} /> */}
-                                        <Route exact path="/dashboard/edit" component={EditEmployee} />
-                                        <Route exact path="/dashboard/employ" component={AddEmployee} />
-                                        <Route exact path="/dashboard/employee-list" component={EmployeeList} />
-                                        <Route exact path="/dashboard" component={Index} />
-                                    </Switch>
-                                </div>
-                   {/* >>>>> FOOTER COMPONENT SECTION <<<<< */}
-                            <Footer />
-                        </div>
-=======
-import DraftTask from './DraftTask';
 import Login from '../../pages/Login';
 
 
@@ -73,11 +39,11 @@ const Dashboard = () =>{
                             <div className="container-fluid dashboard-body-wrapper">
                 {/* >>>>> BODIES COMPONENTS SECTION <<<<< */}
                                 <Switch>
-                                    <Route exact path="/dashboard/draft-task" component={DraftTask} />
+                                    <Route exact path="/dashboard/task" component={Task} />
                                     <Route exact path="/dashboard/profile" component={Profile} />
                                     <Route exact path="/dashboard/billing-report" component={BillingReport} />
                                     <Route exact path="/dashboard/timer-report" component={TimerReport} />
-                                    <Route exact path="/dashboard/task" component={EmployeeTasks} />
+                                    {/* <Route exact path="/dashboard/task" component={EmployeeTasks} /> */}
                                     <Route exact path="/dashboard/edit" component={EditEmployee} />
                                     <Route exact path="/dashboard/employ" component={AddEmployee} />
                                     <Route exact path="/dashboard/employee-list" component={EmployeeList} />
@@ -86,7 +52,6 @@ const Dashboard = () =>{
                             </div>
                 {/* >>>>> FOOTER COMPONENT SECTION <<<<< */}
                         <Footer />
->>>>>>> 1fbe99ac70003d72b25c7a5a1082308f74bd87f0
                     </div>
                 </div>
             </div>
