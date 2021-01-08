f5989a566a8f81833cd8de796fbb5c56ce9159cd
Bastocurrency1999 <bastohenna@gmail.com>
2021-01-08 03:11:44 -0800

    create faq section beneath the pricing card

--- src/components/pages/Pricing.js
+++ src/components/pages/Pricing.js
@@ -69,6 +69,85 @@ const Pricing = () => {
                     </div>
                 </div>
             </div>
+
+            {/*--------------------------- FAQ Section --------------------------------------------------------*/}
+            <div className="container mb-5">
+            <h3 className="text-center text-bold mt-3" style={{color:'#FF6584'}}>Frequently Asked Questions</h3>
+        <div className="d-flex flex-row justify-content-center flex-wrap">
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                        <h6 className="text-white text-center">Can we access pacetimesheet for free?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                            yes! you will have a chance to try pacetimesheet for free. No credit card, contract, or signature is required. But you will be restricted to some features like a workspace for staff, to-do list, and request.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                    <h6 className="text-white text-center">What forms of payment did you accept?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                        we accept all major credit cards - Visa, MasterCard, Paypal. so you can choose the one that better works for you. Also, for every charge you will get full PDF invoice.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                    <h6 className="text-white text-center">Can we run as many projects as we like?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                        Yes you can run as many project as you want as we don't have any limitation. and you will never be charged extra for more projects in  Pacetimesheet.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                    <h6 className="text-white text-center">Does pacetimesheet automatically stop after several minutes of user inactivity?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                        Yes, pacetimesheet automatically stops tracking time when your employees don't perform any activity for 5minutes.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                    <h6 className="text-white text-center">Can we cancel at anytime?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                        Yes you can cancel subscription at anytime. Use how long you want and cancel when you want.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mt-3">
+                <div className="card mb-3 mt-3 ml-3" style={{width:'325px',height:'200px'}}>
+                    <div className="card-header p-3" style={{background:'rgb(51, 94, 234)'}}>
+                    <h6 className="text-white text-center">Can we use this for project management?</h6>
+                    </div>
+                    <div className="card-body p-2 text-justify">
+                        <h6 style={{fontSize:'16px',fontWeight:'normal'}}>
+                        Yes our platform has built in feature where you can track your time on per meeting or project basis. This feature is very useful if you want to bill your client on the basis of hours worked on a task.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
         </>
     )
 }
